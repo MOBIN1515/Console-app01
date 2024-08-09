dbd15f8b5116ac6092aa205ea7eca06371ca98f4
Mobin <Mobin@DESKTOP-NIS2M55>
2024-08-09 15:43:12 +0330

    Add project files.

---
+++ Console app01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Console_app01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            int i = 10;
+            int b = 25;
+            int c = i + b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
